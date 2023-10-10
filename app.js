e8a398e93a127fa10f550b2f648d40c5114a1921
Nicholas Ciraki <Nicholas.Ciraki@jdplc.com>
2023-10-10 11:28:18 +0100

    added more metadata + css

--- app.js
+++ app.js
@@ -10,15 +10,32 @@ app.use(express.static("public"));
 
 app.get('/', async (req, res) => {
   try {
-    // Fetch the EC2 instance ID from the instance metadata http://169.254.169.254/latest/meta-data/instance-id
-    const ec2IdResponse = await axios.get('http://169.254.169.254/latest/meta-data/instance-id');
+    // Fetch the EC2 instance ID from the instance metadata http://169.254.169.254/latest/meta-data/placement/availability-zone
+    const ec2IdResponse = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone');
     const azResponse = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone')
+    const regionResponse = await axios.get('http://169.254.169.254/latest/meta-data/placement/region')
+    const publicHostnameResponse = await axios.get('http://169.254.169.254/latest/meta-data/public-hostname')
+    const publicIpResponse = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4')
+    const localIpResponse = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4')
+
     const instanceId = ec2IdResponse.data;
     const azId = azResponse.data
+    const regionId = regionResponse.data
+    const publicHostnameId = publicHostnameResponse.data
+    const publicIpId = publicIpResponse.data
+    const localIpId = localIpResponse.data
+
 
     // Send the instance ID as a response
     // res.send(`<h1>EC2 Instance ID:</h1><p>${instanceId}</p>`);
-    res.render("index.ejs", {instanceId: instanceId, azId: azId})
+    res.render("index.ejs", {
+      instanceId: instanceId, 
+      azId: azId,
+      regionId: regionId,
+      publicHostnameId: publicHostnameId,
+      publicIpId: publicIpId,
+      localIpId: localIpId,
+    })
   } catch (error) {
     console.error('Error fetching instance ID:', error);
     // res.status(500).send('Error fetching instance ID');
